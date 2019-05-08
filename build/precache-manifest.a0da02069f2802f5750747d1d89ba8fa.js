0e1af542907d413f29155d9af42af4047b8ed027
Janne Mäkelä <jarppe.makela@gmail.com>
2019-05-08 18:03:40 +0300

    frontend buildin lisäys

---
+++ build/precache-manifest.a0da02069f2802f5750747d1d89ba8fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5515a7ca4249e6bd291d",
+    "url": "/static/js/main.98617c8f.chunk.js"
+  },
+  {
+    "revision": "37cfae212e25180f959b",
+    "url": "/static/js/2.0b74813f.chunk.js"
+  },
+  {
+    "revision": "5515a7ca4249e6bd291d",
+    "url": "/static/css/main.2b79e5ee.chunk.css"
+  },
+  {
+    "revision": "4bd75ea4d0a94622648848446c6f0944",
+    "url": "/index.html"
+  }
+];
