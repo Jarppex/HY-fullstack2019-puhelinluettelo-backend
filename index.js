4b181513e729754d98bd702bf1ab56544782b6da
Janne Mäkelä <jarppe.makela@gmail.com>
2019-05-13 13:34:52 +0300

    3.18*: puhelinluettelo ja tietokanta, step5

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// setup
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
@@ -13,50 +14,19 @@ morgan.token('body', function getBody (req) {
 const app = express()
 const Person = require('./models/person')
 
-/*
-let notes = [
-    {
-      "id": 1,
-      "name": "Arto Hellas",
-      "number": "045-1236543",
-    },
-    {
-      "id": 2,
-      "name": "Arto Järvinen",
-      "number": "041-21423123"
-    },
-    {
-      "id": 3,
-      "name": "Lea Kutvonen",
-      "number": "040-4323234"
-    },
-    {
-      "id": 4,
-      "name": "Martti Tienari",
-      "number": "09-784232"
-    }
-]
-*/
-
 app.use(express.static('build'))
-
 app.use(cors())
-
 app.use(bodyParser.json())
-
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-/*
-const generateId = () => {
-    return Math.floor(Math.random() * Math.floor(1000))
-}
-*/
+// GET
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(persons => {
     response.json(persons.map(person => person.toJSON()))
   })
 })
 
+// GET
 app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id)
   .then(person => {
@@ -69,19 +39,26 @@ app.get('/api/persons/:id', (request, response, next) => {
   .catch(error => next(error))
 })
 
+// GET
+app.get('/info', (request, response) => {
+  Person.find({}).then(persons => {
+    const numberOfPersons = persons.length
+    const currentDate = new Date().toString()
+    let info = `Puhelinluettelossa on ${numberOfPersons} henkilon tiedot`
+    response.write(info)
+    response.write('\n')
+    response.write(currentDate)
+    response.end()
+  })
+})
+
+// POST
 app.post('/api/persons', (request, response) => {
     const body = request.body
   
     if (!body.name || !body.number) {
       return response.status(400).json({ error: 'content missing' })
     }
-
-    /*
-    const sameName = notes.find(note => note.name === body.name)
-    if (sameName) {
-      return response.status(403).json({ error: 'name must be unique' })
-    }
-    */
   
     const person = new Person({
       name: body.name,
@@ -91,18 +68,9 @@ app.post('/api/persons', (request, response) => {
     person.save().then(savedPerson => {
       response.json(savedPerson.toJSON())
     })
-  })
+})
 
-app.get('/info', (request, response) => { //KESKEN
-    const numberOfNotes = notes.length
-    const currentDate = new Date().toString()
-    let info = `Puhelinluettelossa on ${numberOfNotes} henkilon tiedot`
-    response.write(info)
-    response.write('\n')
-    response.write(currentDate)
-    response.end()
-  })
-
+// PUT
 app.put('/api/persons/:id', (request, response, next) => {
   const body = request.body
 
@@ -118,34 +86,33 @@ app.put('/api/persons/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+// DELETE
+app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndRemove(request.params.id)
     .then(result => {
       response.status(204).end()
     })
     .catch(error => next(error))
-  })
+})
 
+// olemattomien osoitteiden käsittely
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
-
-// olemattomien osoitteiden käsittely
 app.use(unknownEndpoint)
 
+// virheellisten pyyntöjen käsittely
 const errorHandler = (error, request, response, next) => {
   console.error(error.message)
 
   if (error.name === 'CastError' && error.kind == 'ObjectId') {
     return response.status(400).send({ error: 'malformatted id' })
   } 
-
   next(error)
 }
-
-// virheellisten pyyntöjen käsittely
 app.use(errorHandler)
 
+// portti
 const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
